320e6c8ad40624893edb266a75cefeaf2c576178
Kestutis M <pampyras@gmail.com>
2022-01-03 13:25:51 +0200

    Error caching

--- classes/Api.php
+++ classes/Api.php
@@ -12,44 +12,58 @@ class Api {
     
     private $omniva_api;
     private $prefix;
+    private $config = [];
     
     public function __construct($config = array()) {
+        $this->config = $config;
         $this->omniva_api = new Omniva_api(Helper::get_config_value('api_token', $config, "no_token"), true, false);
         $this->omniva_api->setUrl(Helper::get_config_value('api_url', $config) . "/api/v1/");
         $this->prefix = Helper::get_prefix() . '_api';
     }
     
     public function get_services(){
-        $cache_name = $this->prefix . '_services';
-        $data = get_transient($cache_name);
-        if ($data === false) {
-            $data = $this->omniva_api->listAllServices();
-            set_transient($cache_name, $data, 1800);
-            $last_count = get_option(Helper::get_prefix() . '_total_services', 0);
-            if ($last_count != count($data) && $last_count > 0){
-                update_option(Helper::get_prefix() . '_services_updated', 1);
+        try {
+            $cache_name = $this->prefix . '_services';
+            $data = get_transient($cache_name);
+            if ($data === false) {
+                $data = $this->omniva_api->listAllServices();
+                set_transient($cache_name, $data, 1800);
+                $last_count = get_option(Helper::get_prefix() . '_total_services', 0);
+                if ($last_count != count($data) && $last_count > 0){
+                    update_option(Helper::get_prefix() . '_services_updated', 1);
+                }
+                update_option(Helper::get_prefix() . '_total_services', count($data));
             }
-            update_option(Helper::get_prefix() . '_total_services', count($data));
+        } catch (\Exception $e) {
+            $data = [];
         }
         return $data;
     }
     
     public function get_countries(){
-        $cache_name = $this->prefix . '_countries';
-        $data = get_transient($cache_name);
-        if ($data === false) {
-            $data = $this->omniva_api->listAllCountries();
-            set_transient($cache_name, $data, 1800);
+        try {
+            $cache_name = $this->prefix . '_countries';
+            $data = get_transient($cache_name);
+            if ($data === false) {
+                $data = $this->omniva_api->listAllCountries();
+                set_transient($cache_name, $data, 1800);
+            }
+        } catch (\Exception $e) {
+            $data = [];
         }
         return $data;
     }
     
     public function get_terminals($country = "ALL"){
-        $cache_name = $this->prefix . '_terminals_' . $country;
-        $data = get_transient($cache_name);
-        if ($data === false) {
-            $data = $this->omniva_api->getTerminals($country);
-            set_transient($cache_name, $data, 1800);
+        try {
+            $cache_name = $this->prefix . '_terminals_' . $country;
+            $data = get_transient($cache_name);
+            if ($data === false) {
+                $data = $this->omniva_api->getTerminals($country);
+                set_transient($cache_name, $data, 1800);
+            }
+        } catch (\Exception $e) {
+            $data = [];
         }
         return $data;
     }
