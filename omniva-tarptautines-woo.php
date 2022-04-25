9e72dad160fc9cad5cc4b8206f31594e81edbd25
Kestutis M <kestutis@mijora.lt>
2022-04-25 16:41:26 +0300

    Version update

--- omniva-tarptautines-woo.php
+++ omniva-tarptautines-woo.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Omniva Tarptautines Woocommerce
- * Version: 1.0.1
+ * Version: 1.0.2
  * Plugin URI: https://github.com/mijora
  * Description: Omniva shipping module
  * Author: Mijora
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
   exit;
 }
 
-define('OMNIVA_GLOBAL_VERSION', '1.0.1');
+define('OMNIVA_GLOBAL_VERSION', '1.0.2');
 
 register_activation_hook(__FILE__, array( 'OmnivaTarptautinesWoo\Main', 'activated' ) );
 register_deactivation_hook( __FILE__, array( 'OmnivaTarptautinesWoo\Main', 'deactivated' ) );
